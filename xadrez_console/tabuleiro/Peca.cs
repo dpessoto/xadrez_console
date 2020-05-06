4c0afee4831d8b6a4cf71b21a34d26d14107e038
Daniel Pessoto <d.pessoto@hotmail.com>
2020-05-06 07:53:26 -0300

    Comecando a implementar o tabuleiro

---
+++ xadrez_console/tabuleiro/Peca.cs
@@ -0,0 +1,18 @@
+﻿namespace tabuleiro
+{
+    class Peca
+    {
+        public Posicao posicao { get; set; }
+        public Cor cor { get; protected set; }
+        public int qteMovimentos { get; protected set; } //para saber se o peão já deu o primeiro movimento
+        public Tabuleiro tab { get; set; }
+
+        public Peca(Posicao posicao, Tabuleiro tab, Cor cor)
+        {
+            this.posicao = posicao;
+            this.tab = tab;
+            this.cor = cor;
+            this.qteMovimentos = 0;
+        }
+    }
+}
