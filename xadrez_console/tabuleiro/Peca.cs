8076204600d3f38f1abd7b498466631fe63e7503
Daniel Pessoto <d.pessoto@hotmail.com>
2020-05-06 08:27:15 -0300

    Colocando e criando peças no tabuleiro

--- xadrez_console/tabuleiro/Peca.cs
+++ xadrez_console/tabuleiro/Peca.cs
@@ -7,9 +7,9 @@
         public int qteMovimentos { get; protected set; } //para saber se o peão já deu o primeiro movimento
         public Tabuleiro tab { get; set; }
 
-        public Peca(Posicao posicao, Tabuleiro tab, Cor cor)
+        public Peca(Tabuleiro tab, Cor cor)
         {
-            this.posicao = posicao;
+            this.posicao = null;
             this.tab = tab;
             this.cor = cor;
             this.qteMovimentos = 0;
