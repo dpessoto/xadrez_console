70d4bee0771f8285bd23ba7203671710cb2e4473
Daniel Pessoto <d.pessoto@hotmail.com>
2020-05-12 08:39:14 -0300

    Controlando as pecas capturadas

--- xadrez_console/Program.cs
+++ xadrez_console/Program.cs
@@ -14,15 +14,13 @@ namespace xadrez_console {
                     try {
 
                         Console.Clear();
-                        Tela.imprimirTabuleiro(partida.tab);
-
+                        Tela.imprimirPartida(partida);
+                        /*Tela.imprimirTabuleiro(partida.tab);
                         Console.WriteLine();
-
                         Console.WriteLine("Turno: " + partida.turno);
-                        Console.WriteLine("Aguardando jogada: " + partida.jogadorAtual);
+                        Console.WriteLine("Aguardando jogada: " + partida.jogadorAtual);*/
 
                         Console.WriteLine();
-
                         Console.Write("Origem: ");
                         Posicao origem = Tela.lerPosicaoXadrez().toPosicao();
                         partida.validarPosicaoDeOrigiem(origem);
